87a1a67bcd12f13c169c8ada1baed6b78442db46
Matheus Bruno <matheusferreirabruno@hotmail.com>
2022-03-22 21:37:55 -0300

    Criado model e interface da implementação.

---
+++ RestApi/RestApi/Services/Implementations/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestApi.Model;
+
+namespace RestApi.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person Update(Person person);
+        void Delete(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+
+    }
+}
